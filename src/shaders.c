4d7858249611fc47519c4c40f71f47b1aa265957
RomanDonw <romandonw628@gmail.com>
2026-08-14 16:08:50 +1000

    added example gradient screen filling

--- src/shaders.c
+++ src/shaders.c
@@ -3,25 +3,25 @@
 const char *vertexshadersource = R"(
     #version 330 core
 
-    layout (location = 0) in vec2 _vpos;
+    layout (location = 0) in vec2 vpos;
     
-    out vec2 vpos;
+    out vec2 scrpos;
 
     void main(void)
     {
-        gl_Position = vec4(_vpos.xy, 0.0, 1.0);
-        vpos = _vpos.xy;
+        gl_Position = vec4(vpos, 0.0, 1.0);
+        scrpos = vpos;
     }
 )";
 
 const char *fragmentshadersource = R"(
     #version 330 core
 
-    in vec2 vpos;
+    in vec2 scrpos;
     out vec4 FragColor;
 
     void main(void)
     {
-        FragColor = vec4(0.4, 0.3, 0.2, 1.0);
+        FragColor = vec4((1 + scrpos.x) * 0.5, (1 - scrpos.y) * 0.5, 0.5, 1.0);
     }
 )";
