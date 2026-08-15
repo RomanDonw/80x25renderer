41ed02486ed0a0ac0c2bebc2a3f296f147e2f82f
RomanDonw <romandonw628@gmail.com>
2026-08-15 22:42:54 +1000

    added prototype of vmem_writeacs function to renderfuncs.h header (defined in renderfuncs.c)

--- src/renderfuncs.h
+++ src/renderfuncs.h
@@ -7,5 +7,6 @@
 void vmem_clear(uint16_t *vmem, uint8_t attribute);
 void vmem_writeas(uint16_t *vmem, uint16_t offset, uint8_t attribute, const char *str);
 void vmem_writecs(uint16_t *vmem, uint16_t offset, const char *str);
+void vmem_writeacs(uint16_t *vmem, uint16_t offset, const uint16_t *str);
 
 #endif
